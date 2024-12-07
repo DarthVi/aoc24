373f2748e0f8c5fa5d006d792d6e0aea0a673a69
DarthVi <>
2024-12-07 14:52:33 +0100

    day 2

---
+++ src/day02/day2.ts
@@ -0,0 +1,75 @@
+export async function day2(path: string) {
+  return { part1: await day2_part1(path), part2: await day2_part2(path) };
+}
+
+export async function day2_part1(path: string) {
+  const arr = (await readFile(path)).map((entry) =>
+    entry.map((num) => parseInt(num)),
+  );
+  return arr
+    .map((entry) =>
+      monotone(entry) && adjacentConstraint(entry, 1, 3)
+        ? (1 as number)
+        : (0 as number),
+    )
+    .reduce((a, b) => a + b);
+}
+
+export async function day2_part2(path: string) {
+  const arr = (await readFile(path)).map((entry) =>
+    entry.map((num) => parseInt(num)),
+  );
+  return arr
+    .map((entry) => {
+      if (monotone(entry) && adjacentConstraint(entry, 1, 3))
+        return 1 as number;
+      else {
+        const splicedArr: number[][] = [];
+        for (let i = 0; i < entry.length; i++)
+          splicedArr.push(entry.toSpliced(i, 1));
+
+        if (
+          splicedArr.some(
+            (spliced) => monotone(spliced) && adjacentConstraint(spliced, 1, 3),
+          )
+        )
+          return 1 as number;
+        else return 0 as number;
+      }
+    })
+    .reduce((a, b) => a + b);
+}
+
+async function readFile(path: string) {
+  const file = Bun.file(path);
+  const text = await file.text();
+  return text
+    .trim()
+    .split("\n")
+    .map((line) => line.trim().split(" "));
+}
+
+export function monotone(arr: number[]): boolean {
+  return (
+    arr.every((entry, index) =>
+      index !== arr.length - 1 ? entry < arr[index + 1] : true,
+    ) ||
+    arr.every((entry, index) =>
+      index !== arr.length - 1 ? entry > arr[index + 1] : true,
+    )
+  );
+}
+
+export function adjacentConstraint(
+  arr: number[],
+  min: number,
+  max: number,
+): boolean {
+  const tol = 0;
+  return arr.every((entry, index) =>
+    index !== arr.length - 1
+      ? Math.abs(entry - arr[index + 1]) >= min &&
+        Math.abs(entry - arr[index + 1]) <= max
+      : true,
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import {day1} from "./day01/day1";
+import { day1 } from "./day01/day1";
+import { day2 } from "./day02/day2";
+
+const day = "day02";
+const inputFile = "input.txt";
 
 (async () => {
-    //const result = await day1_part1('src/day01/input.txt');
-    const result = await day1("src/day01/input.txt");
-    console.log(result);
+  //const result = await day1_part1('src/day01/input.txt');
+  const result = await day2(`src/${day}/${inputFile}`);
+  console.log(result);
 })();
